a54126ff985a4b4f276ce3122dd3a11ddb113f37
Spencer Owen <spencer.owen@cdw.com>
2026-08-20 16:27:08 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo install -m 755 foam foam-run /usr/local/bin/
 mkdir -p ~/.config/containers
-printf '[engine]\ncgroup_manager = "cgroupfs"\n' > ~/.config/containers/containers.conf
-podman run --rm docker.io/library/alpine echo ok
+# printf '[engine]\ncgroup_manager = "cgroupfs"\n' > ~/.config/containers/containers.conf
+# podman run --rm docker.io/library/alpine echo ok
 
 sudo semanage fcontext -a -t container_file_t "/opt/foam/cases(/.*)?"
 sudo restorecon -Rv /opt/foam/cases
